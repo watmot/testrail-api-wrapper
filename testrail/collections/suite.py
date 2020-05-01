7d2383b20d498a9c48cf6e75621e02655bfea140
watmot <tomwatson840@gmail.com>
2020-05-01 10:54:03 +0100

    Added {query_string} to Suite Collection `get` endpoint

--- testrail/collections/suite.py
+++ testrail/collections/suite.py
@@ -9,6 +9,7 @@ class SuiteCollection(GetMixin, BaseCollection):
         'get': 'get_suites/{project_id}{query_string}'
     }
 
-    def get(self, project_id=None):
-        response = self._get(project_id=project_id)
+    def get(self, project_id=None, **parameters):
+        query_string = self._parse_query_string(**parameters)
+        response = self._get(project_id=project_id, query_string=query_string)
         return response
